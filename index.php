75b1af94e4638bdc48cb38555c8b04ff75ae02c6
Wiktoria Krakowian <krakowianw1908@gmail.com>
2025-08-12 15:27:08 +0200

    add basic routing and change paths in .html files

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
 
-echo "<h1>Hello world 💪</h1>";
+require 'Routing.php';
+
+$path = trim($_SERVER['REQUEST_URI'], '/');
+$path = parse_url($path, PHP_URL_PATH);
+
+Routing::get('index', 'DefaultController');
+Routing::get('dashboard', 'DefaultController');
+Routing::run($path);
---
+++ src/controllers/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once 'AppController.php';
+
+class DefaultController extends AppController {
+    public function index() {
+        $this->render('login');
+    }
+
+    public function dashboard() {
+        $this->render('dashboard');
+    }
+}
